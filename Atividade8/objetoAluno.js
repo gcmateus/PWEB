99af8c06e002d04c8ab49d896e09d60877607b8d
gcmateus <45423686+gcmateus@users.noreply.github.com>
2020-10-14 21:46:29 -0300

    Update objetoAluno.js

--- Atividade8/objetoAluno.js
+++ Atividade8/objetoAluno.js
@@ -1,9 +1,19 @@
 var aluno = {
     nome:'Mateus', 
-    ra:'0030481623028'}
+    ra:'0030481623028'};
 
-alert(`O tipo é ${typeof(aluno)}`);
-alert(`As propriedades são \n ${aluno.nome} \n ${aluno.ra} `);
+var aluno2 = new Object();
+aluno2.nome = "Joao";
+aluno2.ra = "0030162302328";
+
+var aluno3 = {};
+aluno3.nome = "Maria"
+aluno3.ra = "0030481323028"
+
+alert(`As propriedades do aluno 1 são \n ${aluno.nome} \n ${aluno.ra} `);
+alert(`As propriedades do aluno 2 são \n ${aluno2.nome} \n ${aluno2.ra} `);
+alert(`As propriedades do aluno 3 são \n ${aluno3.nome} \n ${aluno3.ra} `);
+
 
 
 
